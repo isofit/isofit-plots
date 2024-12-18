434a832d133ab0a4b04d143d4ff9f53df57a1aa0
James Montgomery <J.Montgomery@jpl.nasa.gov>
2024-12-18 14:23:49 -0800

    Added plotext to dependencies to enable terminal plotting

--- isoplots/plots/rtms.py
+++ isoplots/plots/rtms.py
@@ -4,6 +4,7 @@ import click
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
+import plotext
 
 from isofit.radiative_transfer import luts
 
@@ -96,6 +97,7 @@ def RTMs(file,
     input_name="Input",
     compare_name="Compare",
     output=None,
+    terminal=False,
     ):
     """\
     Plots RTMs
@@ -115,6 +117,8 @@ def RTMs(file,
         Name to assign for the comparison LUT
     output : str, default=None
         Path to output directory to save plots to
+    terminal : bool, default=False
+        Converts the matplotlib figure to plotext to be plotted into a terminal
     """
     print(f"Loading {file}")
     rtm = luts.load(file)
@@ -156,7 +160,7 @@ def RTMs(file,
     dims = set(u_rtm.coords) - {"wl"}
     for dim in dims:
         fig = plt.figure(figsize=(30, 10))
-        grid = gridspec.GridSpec(ncols=len(variables), nrows=2, wspace=0.2, hspace=0.2)
+        grid = gridspec.GridSpec(ncols=len(variables), nrows=2 if cmp else 1, wspace=0.2, hspace=0.2)
         fig.suptitle(dim, fontsize=32)
 
         plot(u_rtm, dim, variables, fig, grid, 0, input_name)
@@ -168,6 +172,10 @@ def RTMs(file,
             plt.savefig(file, dpi=200, bbox_inches='tight')
             print(f"Wrote to: {file}")
 
+        if terminal:
+            plotext.from_matplotlib(fig)
+            plotext.show()
+
 
 @click.command(name="RTMs", no_args_is_help=True, help=RTMs.__doc__)
 @click.argument("file")
@@ -176,6 +184,7 @@ def RTMs(file,
 @click.option("-in", "--input-name", default="Input")
 @click.option("-cn", "--compare-name", default="Compare")
 @click.option("-o", "--output")
+@click.option("-t", "--terminal", is_flag=True)
 def cli(**kwargs):
     print("Plotting RTMs")
 
